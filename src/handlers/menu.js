c73fc65b67dea2ffb564e7f88b296ed3242c7b0c
pirialmersus <pirialmersus@gmail.com>
2026-01-31 17:27:42 +0200

    Auto-send auth code from deep link

--- src/handlers/menu.js
+++ src/handlers/menu.js
@@ -1,8 +1,17 @@
 // src/handlers/menu.js
-import {mainKeyboard} from '../keyboard/main.js';
-import {TEXTS} from "../constants/texts.js";
+import { TEXTS } from '../constants/texts.js';
+import { mainKeyboard } from '../keyboard/main.js';
+import { handleGetAuthCode } from './auth.js';
 
 export const handleStart = async ctx => {
-  await ctx.reply(TEXTS.MENU.START, mainKeyboard(ctx)
-  );
+  // Check if user came from mobile app to get auth code
+  const startPayload = ctx.startPayload;
+
+  if (startPayload === 'getcode') {
+    // User came from mobile app - auto-send auth code
+    await handleGetAuthCode(ctx);
+    return;
+  }
+
+  await ctx.reply(TEXTS.MENU.START, mainKeyboard(ctx));
 };
